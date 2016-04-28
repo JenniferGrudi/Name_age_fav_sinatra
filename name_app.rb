e99b1e53415b1664f99149b81bad9344228ef01c
jennifer Grudi <jen.grudi@gmail.com>
2016-04-28 19:14:41 -0400

    adding sessions

--- name_app.rb
+++ name_app.rb
@@ -1,17 +1,18 @@
 require 'sinatra'
+enable :sessions
 
 get '/' do
 	erb :age_name_app
 end
 
 post '/input' do
-	@@name = params[:user_name]
-	erb :age_app, :locals => {:name => @@name}
+	session[:name] = params[:user_name]
+	erb :age_app, :locals => {:name => session[:name]}
 end
 
 post '/age' do
-	@@age = params[:user_age]
-	erb :favorite_numbers, :locals => {:age => @@age, :name => @@name}
+	session[:age] = params[:user_age]
+	erb :favorite_numbers, :locals => {:age => session[:age], :name => session[:name]}
 end	
 
 post '/favorite_numbers' do
@@ -19,10 +20,10 @@ post '/favorite_numbers' do
 	favorite_numbers2 = params[:user_favorite_number2]
 	favorite_numbers3 = params[:user_favorite_number3]
 	sum = favorite_numbers1.to_i + favorite_numbers2.to_i + favorite_numbers3.to_i
-	if sum < @@age.to_i
-		erb :sum_numbers, :locals => {:sum => sum, :fav1 => favorite_numbers1, :fav2 => favorite_numbers2, :fav3 => favorite_numbers3, :result => "That sum is less than your age!"}
+	if sum < session[:age].to_i
+		erb :sum_numbers, :locals => {:sum => sum, :fav1 => favorite_numbers1, :fav2 => favorite_numbers2, :fav3 => favorite_numbers3, :result => "That sum is less than your age!", :name => session[:name]}
 	else
-		erb :sum_numbers, :locals => {:sum => sum, :fav1 => favorite_numbers1, :fav2 => favorite_numbers2, :fav3 => favorite_numbers3, :result => "That sum is greater than your age!"}
+		erb :sum_numbers, :locals => {:sum => sum, :fav1 => favorite_numbers1, :fav2 => favorite_numbers2, :fav3 => favorite_numbers3, :result => "That sum is greater than your age!", :name => session[:name]}
 	end		
 end	
 
